aae18333f1ea597a34b93ac8798cfc2c5338aede
Mr.Zhang <zzp198@163.com>
2023-11-29 02:43:24 +0800

    try auto depoly

--- run.bat
+++ run.bat
@@ -1,6 +1,8 @@
 SET CGO_ENABLED=0
 SET GOOS=linux
 SET GOARCH=amd64
-go build -ldflags="-s -w" -o Crond
 
-wsl ./Crond
+go build -ldflags="-s -w" -o Crond/Crond
+
+@REM ssh root@223.240.111.27
+scp.exe Crond/Crond root@223.240.111.27:/root/Crond
--- run.go
+++ run.go
@@ -2,53 +2,85 @@ package main
 
 import (
 	"context"
+	"embed"
 	"errors"
 	"flag"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"log/slog"
+	"html/template"
+	"io/fs"
+	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"os/signal"
+	"strings"
 	"syscall"
 )
 
+//go:embed frontend/*
+var frontendFS embed.FS
+
+//go:embed resource/*
+var resourceFS embed.FS
+
 func main() {
-	ip := flag.String("ip", "0.0.0.0:8080", "")
+	name, _ := os.Executable()
+	fmt.Println(name)
+
+	ip := flag.String("ip", "0.0.0.0:8888", "")
 	flag.Parse()
 
-	//if len(os.Args) > 1 && strings.ToLower(os.Args[1]) == "daemon" {
-	//	cmd := exec.Command(os.Args[0], append(os.Args[2:], "CronTag")...)
-	//	// 碰到的第一个坑,父进程结束时,会向子进程发送HUP,TERM指令,导致子进程会跟随父进程一块结束.
-	//	// SysProcAttr.Setpgid设置为true,使子进程的进程组ID与其父进程不同.(KILL强杀也可以)
-	//	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-	//	if err := cmd.Start(); err != nil {
-	//		slog.Error(err.Error())
-	//	} else {
-	//		slog.Info(fmt.Sprintf("%s [PID] %d running...\n", os.Args[0], cmd.Process.Pid))
-	//	}
-	//	return
-	//}
+	if len(os.Args) > 1 && strings.ToLower(os.Args[1]) == "daemon" {
+		_ = exec.Command("pkill", "Crond").Run()
+
+		cmd := exec.Command(os.Args[0], os.Args[2:]...)
+		// 碰到的第一个坑,父进程结束时,会向子进程发送HUP,TERM指令,导致子进程会跟随父进程一块结束.
+		// SysProcAttr.Setpgid设置为true,使子进程的进程组ID与其父进程不同.(KILL强杀也可以)
+		cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+		if err := cmd.Start(); err != nil {
+			log.Fatal(err)
+		} else {
+			log.Println(fmt.Sprintf("%s [PID] %d running...", os.Args[0], cmd.Process.Pid))
+		}
+		return
+	}
 
 	//gin.SetMode(gin.ReleaseMode)
 	r := gin.New()
 	r.Use(gin.Recovery(), gin.Logger())
 
-	r.GET("m3u8", func(c *gin.Context) {
+	frontend, err := template.New("").Delims("{%", "%}").ParseFS(frontendFS, "frontend/*")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	r.SetHTMLTemplate(frontend)
+
+	resource, err := fs.Sub(resourceFS, "resource")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	r.StaticFS("/resource", http.FS(resource))
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	r.GET("/m3u8", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "m3u8.html", nil)
 	})
 
 	srv := &http.Server{Addr: *ip, Handler: r}
 	srv.RegisterOnShutdown(func() {
-		slog.Info(fmt.Sprintf("Server is shutting down"))
+		log.Println(fmt.Sprintf("Server is shutting down"))
 	})
 
 	go func() {
 		if err := srv.ListenAndServe(); err != nil {
 			if errors.Is(err, http.ErrServerClosed) {
-				slog.Warn(fmt.Sprintf("Server closed under request"))
+				log.Println(fmt.Sprintf("Server closed under request"))
 			} else {
-				slog.Error(err.Error())
+				log.Println(err)
 			}
 		}
 	}()
@@ -58,8 +90,7 @@ func main() {
 	<-down
 
 	if err := srv.Shutdown(context.Background()); err != nil {
-		slog.Error(err.Error())
+		log.Println(err)
 	}
-
-	slog.Info(fmt.Sprintf("Server has stopped gracefully."))
+	log.Println("Server has stopped gracefully.")
 }
