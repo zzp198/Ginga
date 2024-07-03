2ff5ca81de2cf9bbe06bca3a18e6bb2c5c6b09a9
Mr.Zhang <zzp198@163.com>
2024-07-04 02:25:54 +0800

    add bilibili download

---
+++ util/misc.go
@@ -0,0 +1,58 @@
+package util
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func FormatByte(b uint64) string {
+	const (
+		B = 1 << (10 * iota)
+		KB
+		MB
+		GB
+	)
+
+	var value float64
+	var unit string
+
+	switch {
+	case b >= GB:
+		value = float64(b) / GB
+		unit = "GB"
+	case b >= MB:
+		value = float64(b) / GB
+		unit = "MB"
+	case b >= KB:
+		value = float64(b) / GB
+		unit = "KB"
+	case b >= B:
+		value = float64(b) / GB
+		unit = "B"
+	default:
+		value = float64(b)
+		unit = "B"
+	}
+
+	return fmt.Sprintf("%.2f %s", value, unit)
+}
+
+func HttpGet(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	err = resp.Body.Close()
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
