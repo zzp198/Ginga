5d6dc164ae5e337411b2fa72bdc9a80bbdf611e5
zzp198 <zzp198@163.com>
2024-12-09 16:14:25 +0800

    handel data

--- msg/main.go
+++ msg/main.go
@@ -8,116 +8,100 @@ import (
 	"strings"
 )
 
-func main() {
+func WriteString(w *bufio.Writer, str string) {
+	w.WriteString(str)
+	w.Flush()
+	fmt.Println("\x1b[32m" + str + "\x1b[0m")
+}
 
+func main() {
 	fmt.Println("临时邮件服务器")
-	fmt.Println()
 
 	listener, err := net.Listen("tcp", "0.0.0.0:25")
 	if err != nil {
-		fmt.Printf("Error starting server: %v\n", err)
+		fmt.Println(err)
 		return
 	}
-	defer func(listener net.Listener) { _ = listener.Close() }(listener)
 
-	fmt.Printf("SMTP server is running on port %s\n", "0.0.0.0:25")
+	fmt.Println("运行于0.0.0.0:25")
 
 	for {
+
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Printf("Error accepting client: %v\n", err)
+			fmt.Println(err)
 			continue
 		}
 
-		go Parse(conn)
+		go fmt.Println(Process(conn))
 	}
+
 }
 
-func Parse(conn net.Conn) {
+func Process(conn net.Conn) (data string, err error) {
+	defer conn.Close()
 
 	r := bufio.NewReader(conn)
 	w := bufio.NewWriter(conn)
 
-	// 向客户端发送欢迎消息
-	_, _ = w.WriteString("220 Temporary Email Server by zzp198\r\n")
-	_ = w.Flush()
+	WriteString(w, "220 Temporary Email Server\r\n")
 
-	var data string
-	var readingBody bool
+	var reading bool
 
 	for {
 		line, err := r.ReadString('\n')
+
 		if err != nil {
-			if err != io.EOF {
+			//正常应该在QUIT命令后同时停止发送并断开连接,不会冗余读取
+			if err == io.EOF {
 				fmt.Printf("Error reading from client: %v\n", err)
+				return data, err
 			}
-			break
+			fmt.Println(err)
+			return "", err
 		}
 
-		//line = strings.TrimSpace(line)
-		fmt.Println("C: " + line)
+		fmt.Print(line)
 
-		if readingBody {
-			if line == "." {
-				readingBody = false
-				_, _ = w.WriteString("250 OK\r\n")
-				_ = w.Flush()
-
-				fmt.Println(data)
-				fmt.Println()
-
-				//// 现在邮件发送时包含DKIM,net/mail不支持导致报错,还有各种转码问题,“\t代表换行内容”这样就好处理了
-				//msg, err := mail.ReadMessage(strings.NewReader(data))
-				//if err != nil {
-				//	fmt.Printf("Error parsing mail: %v\n", err)
-				//	return
-				//}
-				//
-				//for key, values := range msg.Header {
-				//	for _, value := range values {
-				//		fmt.Printf("%s: %s", key, value)
-				//	}
-				//}
-				//
-				//body, err := io.ReadAll(msg.Body)
-				//if err != nil {
-				//	fmt.Printf("Error reading body: %v\n", err)
-				//	return
-				//}
-				//
-				//fmt.Println(string(body))
-
+		// 内容部分需要保留开头的\t
+		if reading {
+			if strings.TrimSpace(line) == "." {
+				reading = false
+				WriteString(w, "250 OK")
 			} else {
-				data += line + "\n"
+				data += line
 			}
 			continue
 		}
 
-		switch {
-		case strings.HasPrefix(strings.ToUpper(line), "HELO"):
-			_, _ = w.WriteString("250 Hello\r\n")
-			fmt.Print("S: " + "250 Hello\r\n")
-		case strings.HasPrefix(strings.ToUpper(line), "MAIL FROM"):
-			_, _ = w.WriteString("250 OK\r\n")
-			fmt.Print("S: " + "250 OK\r\n")
-		case strings.HasPrefix(strings.ToUpper(line), "RCPT TO"):
-			_, _ = w.WriteString("250 OK\r\n")
-			fmt.Print("S: " + "250 OK\r\n")
-		case strings.HasPrefix(strings.ToUpper(line), "DATA"):
-			_, _ = w.WriteString("354 End data with <CR><LF>.<CR><LF>\r\n")
-			fmt.Print("S: " + "354 End data with <CR><LF>.<CR><LF>\r\n")
-			readingBody = true
-		case strings.HasPrefix(strings.ToUpper(line), "QUIT"):
-			_, _ = w.WriteString("221 Bye\r\n")
-			_ = w.Flush()
+		line = strings.TrimSpace(line)
 
-			fmt.Print("S: " + "221 Bye\r\n")
-			_ = conn.Close()
-			return
-		default:
-			_, _ = w.WriteString("502 Command not implemented\r\n")
-			fmt.Print("S: " + "502 Command not implemented\r\n")
+		if strings.HasPrefix(strings.ToUpper(line), "HELO") {
+			WriteString(w, "250 OK\r\n")
+			continue
 		}
-		_ = w.Flush()
+
+		if strings.HasPrefix(strings.ToUpper(line), "MAIL FROM") {
+			WriteString(w, "250 OK\r\n")
+			continue
+		}
+
+		if strings.HasPrefix(strings.ToUpper(line), "RCPT TO") {
+			WriteString(w, "250 OK\r\n")
+			continue
+		}
+
+		if strings.HasPrefix(strings.ToUpper(line), "DATA") {
+			reading = true
+			WriteString(w, "354 <CR><LF>.<CR><LF>\r\n")
+			continue
+		}
+
+		if strings.HasPrefix(strings.ToUpper(line), "QUIT") {
+			WriteString(w, "221 OK\r\n")
+			return data, nil
+		}
+
+		WriteString(w, "502 Command not ready\r\n")
 	}
 }
