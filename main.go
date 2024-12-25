3118e9afec209661b6ab903fc92ddc65264dc3f9
Mr.Zhang <129676925+zzp198@users.noreply.github.com>
2024-12-25 17:58:31 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"example/common"
 	"github.com/gin-gonic/gin"
 	"github.com/shirou/gopsutil/host"
 	"github.com/shirou/gopsutil/process"
@@ -10,110 +9,44 @@ import (
 	"os/exec"
 	"runtime"
 	"strings"
-	"time"
 )
-
-var jwt string
 
 func main() {
 
 	r := gin.Default()
 
-	r.LoadHTMLGlob("resource/*")
-
-	r.Use(func(c *gin.Context) {
-		if c.Request.URL.Path == "/" {
-			c.Next()
-		} else {
-			n, err := c.Cookie("jwt")
-			if err == nil && n == jwt {
-				c.Next()
-			} else {
-				c.Redirect(http.StatusTemporaryRedirect, "/")
-				c.Abort()
-			}
-		}
-	})
-
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{})
-	})
-
-	r.POST("/", func(c *gin.Context) {
-
-		username := c.PostForm("username")
-		password := c.PostForm("password")
-
-		if username == "admin" && password == "123456" {
-
-			jwt = time.Now().String()
-
-			c.SetCookie("jwt", jwt, 86400, "/", "", false, false)
-
-			// 返回 JSON 数据，表示登录成功
-			c.JSON(http.StatusOK, gin.H{
-				"status":  "ok",
-				"message": "登录成功",
-			})
+	r.GET("/api/host", func(c *gin.Context) {
+		hi, err := host.Info()
+		if err != nil {
+			c.JSON(200, err)
 			return
 		}
 
-		// 登录失败，返回错误信息
-		c.JSON(http.StatusUnauthorized, gin.H{
-			"status":  "error",
-			"message": "用户名或密码错误",
-		})
-	})
-
-	r.GET("/home", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "home.html", gin.H{})
-	})
-
-	r.GET("/api/info/static", func(c *gin.Context) {
-		hostInfo, err := host.Info()
-
+		pr, err := process.NewProcess(int32(os.Getpid()))
 		if err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"code":  "-1",
-				"error": err.Error(),
-			})
-			return
-		}
-
-		p, err := process.NewProcess(int32(os.Getpid()))
-		if err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"code":  "-1",
-				"error": err.Error(),
-			})
+			c.JSON(200, err)
 			return
 		}
 
 		// 获取进程的内存信息
-		memInfo, err := p.MemoryInfo()
+		mi, err := pr.MemoryInfo()
 		if err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"code":  "-1",
-				"error": err.Error(),
-			})
+			c.JSON(200, err)
 			return
 		}
 
 		c.JSON(http.StatusOK, gin.H{
-			"code": "1",
-			"msg":  "ok",
-
-			"主机名":    hostInfo.Hostname,
-			"上线时间":   hostInfo.Uptime,
-			"操作系统":   hostInfo.Platform,
-			"系统版本":   hostInfo.PlatformVersion,
+			"主机名":    hi.Hostname,
+			"上线时间":   hi.Uptime,
+			"操作系统":   hi.Platform,
+			"系统版本":   hi.PlatformVersion,
 			"内核数":    runtime.NumCPU(),
 			"协程数":    runtime.NumGoroutine(),
-			"物理内存占用": common.FormatByte(memInfo.RSS), // Resident Set Size
+			"物理内存占用": mi.RSS, // Resident Set Size
 		})
 	})
 
-	r.GET("/api/info/dynamic", func(c *gin.Context) {
+	r.GET("/api/host/state", func(c *gin.Context) {
 		cmd := exec.Command("curl", "-s", "https://ipinfo.io/ip")
 		output, err := cmd.Output()
 		if err != nil {
@@ -129,5 +62,5 @@ func main() {
 		})
 	})
 
-	r.Run(":80")
+	_ = r.Run(":8880")
 }
